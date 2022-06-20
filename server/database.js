fe181f131cfe8d95852d9c0ce44a2c91f86af330
EdgarIv0 <edgarivannc2000@gmail.com>
2022-06-19 23:50:46 -0500

    Conexion MongoDB Atlas

--- server/database.js
+++ server/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');//modulo para conectar nodejs con mongodb
 
 //configuramos los parámetros de la base de datos
 //url= 'mongodb://localhost/empleados';
- 
-url='mongodb+srv://sistema_empleados:SistemaEmpleados.2022@cluster0.0yres.mongodb.net/?retryWrites=true&w=majority'
+
+url='mongodb+srv://sistema_empleados:SistemaEmpleados.2022@cluster0.rxowood.mongodb.net/?retryWrites=true&w=majority'
 dbparams = {
     useCreateIndex:true,
     useNewUrlParser:true,
